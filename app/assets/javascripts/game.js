f4a6abcd84e5f4fb0f7f675ab6fd2d2113a5f5ef
sanychcz <alexander@420on.cz>
2013-08-15 10:14:33 +0200

    corrected js

--- app/assets/javascripts/game.js
+++ app/assets/javascripts/game.js
@@ -3,7 +3,7 @@ $(function(){
     if ($(this).data('success') && $(this).data('image'))  {
   	 $(this).html($(this).addClass('picture'));
     } else {
-      $(this).html($(this).addClass('error', 100)).next().delay(5).addClass('normal');
+      $(this).html($(this).addClass('normal', 10));
     } 
     if ($('.picture').length === $("div[data-success='true']").length) {
   		window.location.replace("/winner/show");   		
